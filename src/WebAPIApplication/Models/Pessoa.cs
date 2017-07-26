78bebffe2c8345149a1077da4cbd1b45d651773d
Frederico Oliveira <fredso82@gmail.com>
2017-07-26 16:33:27 -0300

    Classe Pessoa

---
+++ src/WebAPIApplication/Models/Pessoa.cs
@@ -0,0 +1,7 @@
+namespace WebAPIApplication{
+    public class Pessoa{
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
